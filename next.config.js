70d0d8859e40e2a50b0cccc48eee334ded532d03
Sal Camassa <salcamassa@gmail.com>
2022-04-25 19:13:16 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
+  domains: ['localhost']
+
 }
 
 module.exports = nextConfig
